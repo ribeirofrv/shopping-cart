47eb84cefb4a8a73eccbd8c98f13ba83cc244a80
Fran Ribeiro <ribeirofrv@gmail.com>
2022-03-31 21:48:32 -0300

    Cumpre Requisito 2 :smile: - Caique Deuso :rocket:

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Sessão -> Lista de Produtos
+const ol = document.querySelector('.cart__items');
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -13,6 +14,39 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// const cartItemClickListener = (event) => {
+//   
+// };
+
+/** CRIA ELEMENTO DO CARRINHO DE COMPRAS */
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+/** ADICIONA LISTA DE PRODUTOS AO CARRINHO DE COMPRAS */
+const addToCart = (values) => ol.appendChild(createCartItemElement(values));
+
+const getDataFromProduct = async (sku) => {
+  const { id, title, price } = await fetchItem(sku);
+  return { sku: id, name: title, salePrice: price };
+};
+
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const buttonAddEvent = async (event) => {
+  const theTarget = event.target;
+  const node = theTarget.parentNode; // Retorna o elemento pai
+  const sku = getSkuFromProductItem(node);
+  const values = await getDataFromProduct(sku);
+  console.log('button');
+  addToCart(values);
+};
+
+/** CRIA ELEMENTOS DA LISTA DE PRODUTOS À VENDA */
 const createProductItemElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -20,64 +54,21 @@ const createProductItemElement = ({ sku, name, image }) => {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', buttonAddEvent);
 
   return section;
 };
 
-// Sessão -> Carrinho de compras
-// const cartItemClickListener = (event) => {
-//   
-// };
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
-
-/** ADICIONA LISTA DE PRODUTOS AO CARRINHO DE COMPRAS */
-// const getSkuFromProductItem = (item) => item
-
-const getDataFromProduct = async (sku) => {
-  const { id, title, price } = await fetchItem(sku);
-  console.log(`${id} | ${title} | R$${price}`); // por aqui tudo OK
-  return {
-    sku: id,
-    name: title,
-    salePrice: price,
-  };
-};
-/* TRYINNNGGG  */
-const buttonAddEvent = async () => {
-  const sku = document.querySelector('span.item__sku');
-  const ol = document.querySelector('cart__items');
-  const values = await getDataFromProduct(sku.innerText); // depois arrumo
-
-  ol.appendChild(createCartItemElement(values));
-};
-
-/** CRIA LISTA DE PRODUTOS  & ~TENTA ADICIONAR UM EVENTO AO BOTÃO ADICIONAR AO CARRINHO */
+/** CRIA LISTA DE PRODUTOS À VENDA */
 const createSectionProducts = async () => {
   const sectionItems = document.querySelector('.items');
   const { results } = await fetchProducts('computador');
-  const items = Object.values(results);
   
-  items.forEach(({ id, title, thumbnail }) => {
-    const values = {
-      sku: id,
-      name: title,
-      image: thumbnail,
-    };
-    
+  results.forEach(({ id, title, thumbnail }) => {
+    const values = { sku: id, name: title, image: thumbnail };
     sectionItems.appendChild(createProductItemElement(values));
   });
-  
-  const buttonAdd = document.querySelectorAll('.item__add');
-  console.log(buttonAdd);
-  buttonAdd.addEventListener('click', await buttonAddEvent());
 };
 
 window.onload = () => {
